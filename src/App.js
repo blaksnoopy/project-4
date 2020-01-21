376b978bad9d64ff91cec43e5b6a8a466c012d4e
Marquiese Solomon <blaksnoopy@Marquieses-MacBook-Pro.local>
2020-01-21 09:52:21 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PROJECT-IV</h1>
       </header>
+          <div class="container">
+              <section id="content">
+                  <form action="">
+                      <h1>Sign Up Here</h1>
+                      <div>
+                          <input type="text" placeholder="Username" required="" id="username" />
+                      </div>
+                      <div>
+                          <input type="password" placeholder="Password" required="" id="password" />
+                      </div>
+                      <div>
+                          <input type="submit" value="Log in" />
+                          {/* <a href="#">Lost your password?</a>
+                          <a href="#">Register</a> */}
+                      </div>
+                  </form>
+              </section>
+          </div>
     </div>
   );
 }
