070bfd0c3485428017cddbcdaa8b224102eabdfc
Marquiese Solomon <blaksnoopy@Marquieses-MacBook-Pro.local>
2020-01-21 11:54:28 -0800

    user model

---
+++ src/pages/App/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: userService.getUser()
+    };
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({user: null});
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>PROJECT-IV</h1>
+        </header>
+          <div>
+          </div>
+            <Switch>
+              <Route exact path='/signup' render={({ history }) => 
+                <SignupPage
+                  history={history}
+                  handleSignupOrLogin={this.handleSignupOrLogin}
+                />
+              }/>
+              <Route exact path='/login' render={({ history }) => 
+                <LoginPage
+                  history={history}
+                  handleSignupOrLogin={this.handleSignupOrLogin}
+                />
+              }/>
+            </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
